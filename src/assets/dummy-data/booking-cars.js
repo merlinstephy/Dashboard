d3ef2b7c1ce0bb5434127a2b7b863d41b57384d7
merlinstephy <142407176+merlinstephy@users.noreply.github.com>
2023-11-11 01:45:16 +0530

    Add files via upload

---
+++ src/assets/dummy-data/booking-cars.js
@@ -0,0 +1,92 @@
+import car01 from "../images/car-01.png";
+import car02 from "../images/car-02.png";
+import car03 from "../images/car-03.png";
+import car04 from "../images/car-04.png";
+import car05 from "../images/car-05.png";
+import car06 from "../images/car-06.png";
+
+const bookingCars = [
+  {
+    id: "01",
+    carName: "Porshe 718 Cayman S",
+    category: "Audi",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: 90373,
+    imgUrl: car01,
+  },
+  {
+    id: "02",
+    carName: "Porshe 718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 3,
+    rentPrice: 6464,
+    imgUrl: car02,
+  },
+  {
+    id: "03",
+    carName: "Porshe 718 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: 12847,
+    imgUrl: car03,
+  },
+  {
+    id: "04",
+    carName: "Por 71 Cayman S",
+    category: "Coupe",
+    type: "Manual",
+    groupSize: 9,
+    rentPrice: 10343,
+    imgUrl: car04,
+  },
+  {
+    id: "05",
+    carName: "shel 78 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 2,
+    rentPrice: 12000,
+    imgUrl: car05,
+  },
+  {
+    id: "06",
+    carName: "Porshe 718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 5,
+    rentPrice: 8584,
+    imgUrl: car06,
+  },
+  {
+    id: "07",
+    carName: "Porshe 718",
+    category: "Audi",
+    type: "Manual",
+    groupSize: 7,
+    rentPrice: 7000,
+    imgUrl: car01,
+  },
+  {
+    id: "08",
+    carName: "718 Cayman S",
+    category: "Toyota",
+    type: "Manual",
+    groupSize: 4,
+    rentPrice: 4915,
+    imgUrl: car02,
+  },
+  {
+    id: "09",
+    carName: "Porshe 718 Cayman S",
+    category: "Bmw",
+    type: "Manual",
+    groupSize: 5,
+    rentPrice: 109,
+    imgUrl: car03,
+  },
+];
+
+export default bookingCars;
